71884198fef721a03cfe394148ecc11cbb1917e9
project_visk <mishal.u@tiqo.co>
2025-08-29 12:24:23 +0530

    sanity updates

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -32,7 +32,7 @@ function Hero() {
               href="/blog/radiant-raises-100m-series-a-from-tailwind-ventures"
               className="flex items-center gap-1 rounded-full bg-fuchsia-950/35 px-3 py-0.5 text-sm/6 font-medium text-white data-hover:bg-fuchsia-950/30"
             >
-              Radiant raises $100M Series A from Tailwind Ventures
+              Radiant raises $100M Series A from Tailwind Venture
               <ChevronRightIcon className="size-4" />
             </Link>
           }
@@ -48,7 +48,7 @@ function Hero() {
           <div className="mt-12 flex flex-col gap-x-6 gap-y-4 sm:flex-row">
             <Button href="#">Get started</Button>
             <Button variant="secondary" href="/pricing">
-              See pricing
+              See the pricing
             </Button>
           </div>
         </div>
